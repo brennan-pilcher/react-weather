38dbe4386f4eedd3062c97980a5cd81f8e68fc55
Brennan Pilcher <brennan.pilcher@gmail.com>
2023-02-19 21:33:59 -0500

    Refactor the weather data parsing and transformation function to improve readability

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -3,107 +3,99 @@ import moment from "moment";
 import { API_URL, DATE_FORMAT } from "../contants";
 
 const parseWeatherDataResponse = (data) => {
-  let weatherData = {
-    city: data.city,
-    currentTime: data.list[0].dt_txt,
-    currentMain: data.list[0].main,
-    currentWeather: data.list[0].weather
-  };
+    let weatherData = {
+        city: data.city,
+        currentTime: data.list[0].dt_txt,
+        currentMain: data.list[0].main,
+        currentWeather: data.list[0].weather
+    };
+    
+    let days = {};
+    let forecast = [];
+    
+    // list of all available weather data returned from the API (3 hour intervals)
+    for (let weatherDataItem of data.list) {
+        let dateTime = new Date( moment.utc(weatherDataItem.dt_txt).local().format(DATE_FORMAT) );
+        
+        if (days[dateTime.getDate()] === undefined) days[dateTime.getDate()] = {};
+        
+        days[dateTime.getDate()][dateTime.getHours()] = weatherDataItem;
+    }
+    
+    for (let date in days) {
+        let day = {};
+        
+        for (let hour in days[date]) {
+            if (day.date === undefined) {
+                let dateObj = new Date( moment.utc(days[date][hour].dt_txt).local().format(DATE_FORMAT) );
+                day["date"] = {
+                    day: dateObj.getDay(),
+                    date: dateObj.getDate(),
+                    month: dateObj.getMonth()
+                };
+            }
 
-  let days = {};
+            const minTempNotSet = day.temp_min === undefined;
+            const maxTempNotSet = day.temp_max === undefined;
+            const descriptionNotSet = day.short_description === undefined;
 
-  // list of all available weather data returned from the API (3 hour intervals)
-  for (let elem of data.list) {
-      let now = new Date( moment.utc(elem.dt_txt).local().format(DATE_FORMAT) );
+            const minimumTempForCurrentHour = days[date][hour].main.temp_min;
+            const maximumTempForCurrentHour = days[date][hour].main.temp_max;
 
-      if (days[now.getDate()] === undefined) {
-          days[now.getDate()] = {};
-      }
+            const currentHourHasLowerTempThanMin = day.temp_min > minimumTempForCurrentHour;
+            const currentHourHasHigherTempThanMax = day.temp_max < maximumTempForCurrentHour;
 
-      days[now.getDate()][now.getHours()] = elem;
-  }
-
-  let forecast = [];
-
-
-  // loop through all available days and construct a day object for each one with info relevant to the day's forecast
-  for (let date in days) {
-      let day = {};
-
-      for(let hour in days[date]) {
-
-          // set the date object within the current day
-          if (day.date === undefined) {
-              let dateObj = new Date( moment.utc(days[date][hour].dt_txt).local().format(DATE_FORMAT) );
-              day["date"] = {
-                  day: dateObj.getDay(),
-                  date: dateObj.getDate(),
-                  month: dateObj.getMonth()
-              };
-          }
-          // set the day's minimum temperature
-          if (day.temp_min === undefined || day.temp_min > days[date][hour].main.temp_min) {
-              day["temp_min"] = days[date][hour].main.temp_min;
-          }
-          //set the day's maximum temperature
-          if (day.temp_max === undefined || day.temp_max < days[date][hour].main.temp_max) {
-              day["temp_max"] = days[date][hour].main.temp_max;
-          }
-          // set the day's weather description
-          // TODO add priority list for escalating conditions, e.g. snow trumps rain, rain trumps clouds, etc
-          if(day.short_description === undefined) {
-              day["short_description"] = days[date][hour].weather[0].main;
-          }
-      }
-
-      forecast.push(day);
-
-  }
-
-  return {
-    weatherData,
-    forecast,
-    ready: true
-  };
+            if (minTempNotSet || currentHourHasLowerTempThanMin) day["temp_min"] = minimumTempForCurrentHour;
+            if (maxTempNotSet || currentHourHasHigherTempThanMax) day["temp_max"] = maximumTempForCurrentHour;
+            // TODO add priority list for escalating conditions, e.g. snow trumps rain, rain trumps clouds, etc
+            if (descriptionNotSet) day["short_description"] = days[date][hour].weather[0].main;
+        }
+        
+        forecast.push(day);
+    }
+    
+    return {
+        weatherData,
+        forecast,
+        ready: true
+    };
 }
 
 export const useWeatherData = (location) => {
-  console.log("(hook) location: ", location)
-  const [data, setData] = useState({
-    weatherData: {
-      city : {},
-      currentWeather : {},
-      currentMain : {},
-      currentTime : ''
-    },
-    forecast: [],
-    ready : false
-  });
-  
-  useEffect(() => {
-    const fetchBody = {
-      type: location.type,
-      [location.type]: location.location
-    };
-
-    const fetchWeatherData = async () => {
-      const response = await fetch(
-        API_URL,
-        {
-          method: 'POST',
-          body: JSON.stringify(fetchBody)
+    const [data, setData] = useState({
+        weatherData: {
+            city : {},
+            currentWeather : {},
+            currentMain : {},
+            currentTime : ''
+        },
+        forecast: [],
+        ready : false
+    });
+    
+    useEffect(() => {
+        const fetchBody = {
+            type: location.type,
+            [location.type]: location.location
+        };
+        
+        const fetchWeatherData = async () => {
+            const response = await fetch(
+                API_URL,
+                {
+                    method: 'POST',
+                    body: JSON.stringify(fetchBody)
+                }
+            );
+                
+            const jsonResponse = await response.json();
+            const weatherData = parseWeatherDataResponse(jsonResponse.data);
+            
+            setData(weatherData);
         }
-      );
-
-      const jsonResponse = await response.json();
-      const weatherData = parseWeatherDataResponse(jsonResponse.data);
-      console.log("weather data!", weatherData)
-
-      setData(weatherData);
+            
+        fetchWeatherData();
+    }, [location.type, location.location]);
+        
+        return data;
     }
-
-    fetchWeatherData();
-  }, [location.type, location.location]);
-
-  return data;
-}
